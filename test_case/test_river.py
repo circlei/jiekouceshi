ba783033e84c9494bedc9040f311757dbe771a64
circlei <75718147+circlei@users.noreply.github.com>
2020-12-25 16:53:14 +0800

    Update test_river.py

--- test_case/test_river.py
+++ test_case/test_river.py
@@ -4,27 +4,9 @@
 # @Time      :2020/12/25 10:46
 # @Author    :SHUI
 import pytest,allure
-from common.test_jk import Water_Api
 from data.datas import river_case
 import os
 import logging#日志记录用户的动作
-#Water_Api().log_info
-'''
-class Test_River(object):
-    aaa=Water_Api().river
-    @allure.story('这是story')
-    @allure.title('这title')
-    @allure.severity
-    @pytest.mark.parametrize("key,river_name",river_case("../data/shujv.yaml"))
-    def test_1(self,key,river_name):
-        res = self.aaa(key,river_name)
-        try:
-            assert "成功的返回" in res
-            logging.info('有效的输入返回')
-        except:
-            assert "成功的返回" not in res
-            logging.info('无效的输入返回')
-'''
 
 def test_1(water):
     try:
@@ -36,7 +18,7 @@ def test_1(water):
 
 
 if __name__ == '__main__':
-    #pytest.main(['-s'])
+
     pytest.main(['-s','--alluredir','../temp'])
     os.system('allure generate ../temp -o ../report --clean')
 
